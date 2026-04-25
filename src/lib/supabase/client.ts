28ec9a96c91586376db586258a7584880dc8f45a
moomoocow <kwangmook.dev@gmail.com>
2026-04-25 14:17:25 +0900

    chore: reset project

--- src/lib/supabase/client.ts
+++
@@ -1,9 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-
-import { getSupabaseEnv } from "./env";
-
-export function createClient() {
-  const { url, publishableKey } = getSupabaseEnv();
-
-  return createBrowserClient(url, publishableKey);
-}
